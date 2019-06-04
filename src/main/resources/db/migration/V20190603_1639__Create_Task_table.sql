80aa611e450c2379942f35974296c11c9b8bde10
Liao Xueqing <xueqingliao@foxmail.com>
2019-06-04 09:32:45 +0800

    Add config for flyway and verify db.migration work well

--- src/main/resources/db/migration/V20190603_1639__Create_Task_table.sql
+++ src/main/resources/db/migration/V20190603_1639__Create_Task_table.sql
@@ -1,6 +1,5 @@
-CREATE TABLE `book` (
+CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
